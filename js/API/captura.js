6e8ec177023e440b62cdf27cc441d72a22815021
JuanCarlo VO <yeankarlo@gmail>
2013-03-02 13:32:16 -0600

    asd

--- js/API/captura.js
+++ js/API/captura.js
@@ -1,21 +1,30 @@
 // Captura Foto
 function capturaImg() {
 	// start image capture
-	var ruta = navigator.device.capture.captureImage(captureSuccess, captureError, {limit:1});
-	
-	// Cambiamos de color el botón de tomar la foto
-	$('#regFoto').attr('rel',mediaFiles[0].fullPath).css('background-color','#0F0');
-	
-	// Ponemos la foto debajo del botón "Tomar foto" y arriba del botón "Envar"
-	$('#login div[data-role=content] #regSubmit').prepend('<img src="'+ruta+'" style="width:100%" />');
+	// navigator.device.capture.captureImage(captureSuccess, captureError, {limit:1});
+	navigator.device.capture.captureImage(
+		function(mediaFiles) {
+			pgAlert('Captura de foto', 'Foto tomada satisfactoriamente.');
+			
+			// Cambiamos de color el botón de tomar la foto
+			$('#regFoto').attr('rel',mediaFiles[0].fullPath).css('background-color','#0F0');
+			
+			// Ponemos la foto debajo del botón "Tomar foto" y arriba del botón "Enviar"
+			$('#login div[data-role=content] #regSubmit').prepend('<img src="'+mediaFiles[0].fullPath+'" style="width:100%" />');
+		}, 
+		function(error) {
+			pgAlert('Captura de foto', 'Error al tomar foto.');
+		}, 
+		
+		{limit:1});
 }
 
-// capture callback
+// capture callback -Se asigna una función a la variabe
 var captureSuccess = function(mediaFiles) {
 	return mediaFiles[0].fullPath;
 };
 
-// capture error callback
+// capture error callback -Se asigna una función a la variable
 var captureError = function(error) {
-	return 'Error al tomar la foto'
+	return 'Error al tomar la foto';
 };
